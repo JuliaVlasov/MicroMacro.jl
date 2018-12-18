44a19bf10576cfa4799edb8d0e409d9113de7989
Pierre Navaro <navaro@math.cnrs.fr>
2018-12-18 22:19:17 +0100

    Update C1.jl

--- test/C1.jl
+++ test/C1.jl
@@ -1,13 +1,19 @@
-function C1(t, fft_u, fft_v, A1, A2, matr, conjmatr, sigma, 
+function C1(t, fft_u, fft_v, A1, A2, 
+	    matr, conjmatr, sigma, 
             llambda, Ktaubis, epsilon, Ntaumm)
 
-    champu, champv = ftau(t, fft_u, fft_v, A1, A2, matr, conjmatr, sigma, llambda)
+    champu, champv = ftau(t, fft_u, fft_v, A1, A2, 
+			  matr, conjmatr, sigma, llambda)
 
     champu_fft = fft(champu, dims=1)
     champv_fft = fft(champv, dims=1)
 
-    champu_fft[1, :].fill(0)
-    champv_fft[1, :].fill(0)
+    n = size(champu)[2]
+
+    for i in 1:n
+        champu_fft[1, i] = 0.0
+        champv_fft[1, i] = 0.0
+    end
 
     champu = ifft(champu_fft ./ (1im * Ktaubis), dims=1)
     champv = ifft(champv_fft ./ (1im * Ktaubis), dims=1)
