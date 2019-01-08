f255c74c54d682a89508366b2b7b2beccd5ae5ca
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-01-08 14:33:22 +0100

    Update reconstr_x.jl

--- test/reconstr_x.jl
+++ test/reconstr_x.jl
@@ -1,17 +1,17 @@
 function reconstr_x(u, x, xmin, xmax)
 
     ntau  = size(u)[1]
-    nx    = size(u)[2]
+    nx    = size(x)[1]
     L     = xmax - xmin
-    UA    = zeros(ComplexF64, ntau)
+    UA    = zeros(ComplexF64, (ntau,nx))
     v     = fft(u) / nx
 
-    for j in 1:ntau÷2
-        UA .+= v[:, j] .* exp.(1im * 2π / L * (j-1) * (x .- xmin))
+    for j in 1:nx÷2
+        UA .+= v[:, j] .* exp.(1im * 2π / L * (j-1) * (x' .- xmin))
     end
 
-    for j in ntau÷2+1:ntau
-        UA .+= v[:, j] .* exp.(1im * 2π / L * (j-1-nx) * (x .- xmin))
+    for j in nx÷2+1:nx
+        UA .+= v[:, j] .* exp.(1im * 2π / L * (j-1-nx) * (x' .- xmin))
     end
 
     UA
