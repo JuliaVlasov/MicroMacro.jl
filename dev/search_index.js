f65729400388236fe7f3a18a50b2d69a110d3e8a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-13 09:55:36 +0000

    build based on 6d1421b

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Documentation",
+    "title": "Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#MicroMacro.jl-Documentation-1",
+    "page": "Documentation",
+    "title": "MicroMacro.jl Documentation",
+    "category": "section",
+    "text": ""
+},
+
+]}
