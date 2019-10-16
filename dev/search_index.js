c76fddceb6bf09abb52f9b407faa9c4826f0d34a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-16 15:03:20 +0000

    build based on e11118f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,19 +1,3 @@
-var documenterSearchIndex = {"docs": [
-
-{
-    "location": "#",
-    "page": "MicroMacro.jl",
-    "title": "MicroMacro.jl",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "#MicroMacro.jl-1",
-    "page": "MicroMacro.jl",
-    "title": "MicroMacro.jl",
-    "category": "section",
-    "text": "Documentation for MicroMacro.jl"
-},
-
-]}
+var documenterSearchIndex = {"docs":
+[{"location":"#MicroMacro.jl-1","page":"MicroMacro.jl","title":"MicroMacro.jl","text":"","category":"section"},{"location":"#","page":"MicroMacro.jl","title":"MicroMacro.jl","text":"Documentation for MicroMacro.jl","category":"page"}]
+}
